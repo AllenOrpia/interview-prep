57058c9b32f7c618904de21f868ad275ab38de22
Allen Orpia <allenorpia17@gmail.com>
2024-06-25 18:21:37 -0700

    added more blind75 practice problems

--- blind75/trees.js
+++ blind75/trees.js
@@ -1,5 +1,5 @@
 
-// ? Maximum depth of Binary Tree
+// ? Maximum depth of Binary Tree - 
 
 /*
     * Can use a classic dfs approach
@@ -19,5 +19,36 @@ const maxDepth = (root) => {
 
     // After going down both branches of the current root, we return their largest number and bubble it up to the very top
     return Math.max(left, right);
+
+}
+
+
+
+// ? Same Tree - Given the roots of two binary trees p and q, write a function to check if they are the same or not
+
+/*
+    * Can use a classic dfs approach for both binary trees
+    * As we traverse we check if both nodes are equal
+    * If they are we continue
+    * otherwise we return false
+
+
+*/
+
+
+const isSame = (p, q) => {
+    // base cases
+    if (!p && !q) return true;
+    if (!p || !q || p !== q) return false
+ 
+   
+
+    // Recursive case
+    const left = isSame(p.left, q.left);
+    const right = isSame(p.right, q.right);
+
+    // After traversing every node we return if its true that they are the same
+    // If both branches are true then the trees are the same, if one returns false then they are not the same
+    return left && right;
 
 }
