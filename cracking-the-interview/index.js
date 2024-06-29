3d6272871b2b7b094ad0f90e0c7fe5a1439c2dcf
Allen Orpia <allenorpia17@gmail.com>
2024-06-28 18:37:08 -0700

    added more practice

--- cracking-the-interview/index.js
+++ cracking-the-interview/index.js
@@ -46,3 +46,58 @@ const isUnique2 = (str) => {
 
     return true
 }
+
+
+
+/*
+    ? Check Permutation - Given two strings, write a method to decide if one is a permutation of the other
+
+    ! Basically look for an anagram of one string in the other string
+        ! Hints of using a sliding window problem
+
+    * Brute force 
+        - I can use a hash to store the values of the first string and its counters
+        - I can then iterate through the second string 
+        - At every index I look at a window that is of the same length as the first string
+        - For each window I create a temporary copy of the first strings hashmap counter
+        - I iterate over that window and decrement the counters at the new hash
+        - If the new counter reaches the end of the window without breaking then I found a matching string permutation
+
+        - At the very end I return a false, 
+
+
+    * 
+
+    ! I can sort the 2 strings and iterate and compare both strings at that point
+
+
+*/
+
+const checkPermutation = (s1, s2) => {
+    const hash = {};
+
+    for (let c of s1) {
+        if (hash[c]) hash[c]++
+        else hash[c] = 1;
+    }
+
+    let m = s2.length; 
+    let n = s1.length;
+
+    for (let i = 0; i <= m - n; i ++) {
+        let temp = { ...hash };
+        let j = 0;
+
+        // Iterate over the new window and decrement the counters
+        for (j = i; j < i + n; j++) {
+            let c = s2[j];
+            if (temp[c] && temp[c] > 0) temp[c]--
+            else break
+         }
+
+        //  If j === i + n, that means we were able to go through the whole window, that means there were matching character counts, we return an early true
+        if (j === i + n) return true
+    }
+
+    return false;
+}
